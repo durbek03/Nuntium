f0988323f519bda69b5ec5db575645e1c8306e35
durbek03 <ataboyevdurbek@gmail.com>
2022-03-14 17:41:58 +0500

    Stuck in LazyRow

---
+++ app/src/main/java/com/example/nuntium/constants/Constants.kt
@@ -0,0 +1,19 @@
+package com.example.nuntium.constants
+
+object Constants {
+    const val API_KEY = "b8445f728c1846319619d8a18f2fefef"
+    val TOPICS = listOf<String>(
+        "Sport",
+        "Politics",
+        "Life",
+        "Gaming",
+        "Animals",
+        "Nature",
+        "Food",
+        "Art",
+        "History",
+        "Fashion"
+    )
+    const val BASE_URL = "https://newsapi.org/v2/"
+    const val PAGE_SIZE = 20
+}
